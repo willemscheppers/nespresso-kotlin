2ddcacd31e21edfe67dba31758c0fd79f53835a0
ventouristeam <ventouristeam@gmail.com>
2017-11-07 19:58:02 +0100

    Added junit 5 to the classpath and an empty test for CoffeeApplicationService.

---
+++ src/test/kotlin/net/scriptgate/kotline/nespresso/application/CoffeeApplicationServiceTest.kt
@@ -0,0 +1,12 @@
+package net.scriptgate.kotline.nespresso.application
+
+import net.scriptgate.kotlin.nespresso.application.CoffeeApplicationService
+import org.junit.jupiter.api.Test
+
+class CoffeeApplicationServiceTest {
+    @Test
+    internal fun purchaseCoffee() {
+        val service = CoffeeApplicationService()
+        
+    }
+}
